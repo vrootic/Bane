ea85844eff87740f13e86baed9dbcf99a0118cba
Vic <vrootic@gmail.com>
2015-06-10 14:07:39 +0800

    Testing QLineEdit widget

--- mainlayout.py
+++ mainlayout.py
@@ -8,9 +8,32 @@ from calculator import Calculator
 
 app = QApplication(sys.argv)
 layout = QHBoxLayout()
-calculator = Calculator()
-layout.addWidget(calculator)
-calculator.show()
+spinBox = QLineEdit()
+result = QLabel("$")
+ylayout = QVBoxLayout()
+ylayout.addWidget(result)
+spinBox.resize(200, 50)
+addBtn = QPushButton("&Add")
+layout.addWidget(spinBox)
+layout.addWidget(addBtn)
+ylayout.addLayout(layout)
+
+def adder():
+    tmp = spinBox.selectedText()
+    print tmp
+    #tmp = tmp + 1
+    result.setText(str(tmp))
+
+addBtn.clicked.connect(adder)
+#layout.addStretch(1)
+
+
+widget = QWidget()
+widget.setLayout(ylayout)
+widget.show()
+#calculator = Calculator()
+#layout.addWidget(calculator)
+#calculator.show()
 
 if __name__ == '__main__':
     sys.exit(app.exec_())
