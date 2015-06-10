d12539318c586488680335c416e628501a69bd9b
Vic <vrootic@gmail.com>
2015-06-10 10:11:26 +0800

    1. Change the createLayout method, split it into handleLayout() and createBaseInputLayout() for future use 2. We couldn't update the Calculator layout for unknown reasons.

--- calculator.py
+++ calculator.py
@@ -59,6 +59,42 @@ class Calculator(QWidget):
         self.base.setValue(1)
         self.calculate()
 
+    def createBaseInput(self):
+        newBaseInput = QSpinBox()
+        newBaseInput.setRange(1, 20000)
+        self.bases.append(newBaseInput)
+        self.addBaseInputToLayout()
+    
+    def addBaseInputToLayout(self):
+        pass
+
+    def handleLayout(self):
+        self.h1 = QHBoxLayout()
+        self.v1 = QVBoxLayout()
+        self.v2 = QVBoxLayout()
+        self.v1.addWidget(self.beginDateLabel)
+        self.v1.addWidget(self.beginDate)
+        self.v1.addWidget(self.endDateLabel)
+        self.v1.addWidget(self.endDate)
+        self.v1.addWidget(self.baseLabel)
+        
+        self.v2.addWidget(self.base)
+        for w in self.bases:
+            self.v2.addWidget(w)
+        self.h1.addLayout(self.v2)
+        self.h1.addWidget(self.addButton)
+        
+        self.v1.addLayout(self.h1)
+        self.v1.addWidget(self.resultLabel)
+        self.v1.addWidget(self.result)
+        self.v1.addWidget(self.resetButton)
+        self.v1.addWidget(self.quitButton)
+        self.layout = QVBoxLayout(self)
+        self.layout.addLayout(self.v1)
+        
+        self.setLayout(self.layout)
+        self.setWindowTitle("Calculator")
+        self.resize(300, 300)
 
     def createLayout(self):
 
@@ -82,36 +118,23 @@ class Calculator(QWidget):
         self.base = QSpinBox()
         self.base.setRange(1, 20000)
         self.baseLabel.setBuddy(self.base)
-        
+        self.addButton = QPushButton("&Add record")
+        self.bases = []
+         
         self.resetButton = QPushButton("&Reset")
         self.quitButton = QPushButton("&Quit")
         
         self.resultLabel = QLabel("Result:")
-        self.result = QLabel("$")
+        self.result = QLabel("$0.0")
 
-        v1 = QVBoxLayout()
-        v1.addWidget(self.beginDateLabel)
-        v1.addWidget(self.beginDate)
-        v1.addWidget(self.endDateLabel)
-        v1.addWidget(self.endDate)
-        v1.addWidget(self.baseLabel)
-        v1.addWidget(self.base)
-        v1.addWidget(self.resultLabel)
-        v1.addWidget(self.result)
-        v1.addWidget(self.resetButton)
-        v1.addWidget(self.quitButton)
-        v1.addStretch(1)
-        
-        layout = QVBoxLayout(self)
-        layout.addLayout(v1)
-        
-        self.setLayout(layout)
-        self.resize(300, 300)
+        self.handleLayout()
+
 	
     def createConnection(self):
         self.beginDate.dateChanged.connect(self.calculate)
         self.endDate.dateChanged.connect(self.calculate)
         self.base.valueChanged.connect(self.calculate)
+        self.addButton.clicked.connect(self.createBaseInput)
         self.resetButton.clicked.connect(self.resetAll)
         self.quitButton.clicked.connect(self.close)
         
